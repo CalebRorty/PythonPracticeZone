040eae4b40ffabee6520d3096e2b3b34dc52ff22
Rorty <carortv@gmail.com>
2018-10-19 23:16:14 -0500

    'accidentally' Made a number conversion file in Test kitchen, tommow will upload it to its own file and expand on it as its a cool program

--- TestKitchen.py
+++ TestKitchen.py
@@ -7,25 +7,26 @@ ones = {1:'One', 2:'Two', 3:'Three', 4:'Four', 5:'Five', 6:'Six', 7:'Seven', 8:'
 tens = {2:'Twenty', 3:'Thirty', 4:'Forty', 5:'Fifty', 6:'Sixty', 7:'Seventy', 8:'Eighty', 9:'Ninety'}
 special = {10:'Ten', 11:'Eleven', 12:'Twelve', 13:'Thirteen', 14:'Fourteen', 15:'Fifteen', 16:'Sixteen', 17:'Seventeen', 18:'Eighteen', 19:'Nineteen'}
 
-def numberToWord(number):
-        if number < 10:
-            for key in ones:
-                if number == key:
-                    return (ones[number])
-        elif number == 10:
-            return special[10]
-        elif (number > 10 and number < 20):
-            for key in special:
-                if number == key:
-                    return (special[number])
-        elif number >= 20:
-            for i in tens:
-                if number == (tens):
-                    for i in number:
-                        return False
+def numberToWord():
+    number = input("What Number Would You Like To Convert? ")
+    number = int(number)
+    if number < 10:
+        for key in ones:
+            if number == key:
+                return (ones[number])
+    elif number == 10:
+        return special[10]
+    elif (number > 10 and number < 20):
+        for key in special:
+            if number == key:
+                return (special[number])
+    elif number >= 20:
+        nNumber = number // 10
+        rNumber = number % 10
+        for i in tens:
+            if nNumber == i:
+                return (tens[nNumber] + '-' + ones[rNumber])
 
-
-
-print (numberToWord(22))
+print (numberToWord())
 
 #But If We Want The Reverse Of This
