0b229669aaf4bfe1a4740021ebe57bc5fdd4ba2f
Rorty <carortv@gmail.com>
2018-10-22 17:24:15 -0500

    Created program that can remove vowels from given sentence

---
+++ RemoveVowels.py
@@ -0,0 +1,23 @@
+"""This is a program that asks for a sentence, and removes
+   all of the vowels (excluding y) from said sentence"""
+
+vowels = ['a','e','i','o','u']
+
+
+def userInfo():
+    userString = input('Please enter a sentence: ')
+    userString = userString.lower()
+
+    return userString
+
+def vowelRemove(string):
+    stringLength = len(string)
+    for i in range(0,(stringLength+1)):
+        letter = (string[(i-1):i])
+        for iV in vowels:
+            if iV == letter:
+                print (letter)
+                string = string.replace(letter,'')
+
+    print (f'If we remove all of the vowels we are left with "{string}"')
+print(vowelRemove(userInfo()))
