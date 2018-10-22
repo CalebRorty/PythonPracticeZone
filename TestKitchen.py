d5c6bbadbfaad768514f223f121ebcfbddd66412
Rorty <carortv@gmail.com>
2018-10-22 17:25:01 -0500

    nothing special was just doing some test's in the kitchen

--- TestKitchen.py
+++ TestKitchen.py
@@ -1,10 +1,23 @@
 """This is my playground Where I mess around with code to see if my ideas
 work without creating a bunch of random code inside a project"""
 
-import pygame
+vowels = ['a','e','i','o','u']
 
-size = (1020, 720)
-screen = pygame.display.set_mode(size)
-pygame.display.set_caption("Test Title Test")
-while True:
-    print (screen)
+
+def userInfo():
+    userString = input('Please enter a sentence: ')
+    userString = userString.lower()
+
+    return userString
+
+def vowelRemove(string):
+    stringLength = len(string)
+    for i in range(0,(stringLength+1)):
+        letter = (string[(i-1):i])
+        for iV in vowels:
+            if iV == letter:
+                print (letter)
+                string = string.replace(letter,'')
+
+    print (f'If we remove all of the vowels we are left with "{string}"')
+print(vowelRemove(userInfo()))
