3f0c2e7c628395e79e0342a3cd50cddc90391c9f
Rorty <carortv@gmail.com>
2018-10-23 16:36:40 -0500

    added a new project one

---
+++ BetterPracticeProjects.org/Project1.py
@@ -0,0 +1,6 @@
+name = input("What is your name: ")
+age = input("What is your age: ")
+print ('hello ' + name + ', you are ' + age + ' years old')
+tillOH = 100 - int(age)
+tillOH = str(tillOH)
+print (f'you will turn 100 in ' + tillOH + ' years')
