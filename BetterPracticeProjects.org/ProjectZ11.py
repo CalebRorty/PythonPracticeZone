8fd48df0f44f0f435427312061ab8cbe4625b036
Rorty <carortv@gmail.com>
2018-10-24 13:53:19 -0500

    Changed % to /

--- BetterPracticeProjects.org/ProjectZ11.py
+++ BetterPracticeProjects.org/ProjectZ11.py
@@ -3,8 +3,8 @@ num = int(input("Pleae Enter a number: "))
 lNum = num + 1
 a = []
 
-[(a.append(i)) for i in range(1,lNum) if num % i != 0 ]
-if len(a) > 2:
+[(a.append(i)) for i in range(1,lNum) if num / i != 0 ]
+if len(a) > 1:
     print ("This # Is Prime")
 else:
     print ("This # Not Prime")
