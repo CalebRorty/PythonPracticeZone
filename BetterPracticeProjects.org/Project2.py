29ced1080273241f7ff131ddad132a0a84adf413
Rorty <carortv@gmail.com>
2018-10-24 09:40:50 -0500

    Optimized

--- BetterPracticeProjects.org/Project2.py
+++ BetterPracticeProjects.org/Project2.py
@@ -1,3 +1,2 @@
-userNumber = input("Please give a number: ")
-userNumber = int(userNumber)
+userNumber =  int(input("Please give a number: "))
 print ("That Is Even" if userNumber % 2 == 0 else "That Is Odd")
