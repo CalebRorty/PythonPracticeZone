ab89e9492c7bdabb5f0c996b03c3f359741d2e7d
Rorty <carortv@gmail.com>
2018-10-24 10:26:47 -0500

    Optimized

--- BetterPracticeProjects.org/Project3.py
+++ BetterPracticeProjects.org/Project3.py
@@ -3,3 +3,12 @@ b = []
 [(b.append(i)) for i in a if i < 5]
 
 print (b)
+
+"""-----------Written In One Line-----------"""
+
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+[(a.remove(i)) for i in a[::-1] if i >= 5]
+
+print (a)
+
+#technicaly 3
